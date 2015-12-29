c22e961ebdf111f3adfbee8f3de5e7fb32fa3097
Equan Pr <equan.p@gmail.com>
2015-12-30 06:15:58 +0700

    Use ES6 promise

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 'use strict'
 
-var paribasan_jawa = require('./paribasa.json')
-var si = require('search-index')()
+const si = require('search-index')()
+let paribasan_jawa = require('./paribasa.json')
 
 function Paribasan () {
   // lapo iki?
 }
 
-Paribasan.prototype.indexing = function (callback) {
-  var paribasan_jawa_array = Paribasan._toArray(paribasan_jawa)
-  si.add(paribasan_jawa_array, function (err) {
-    err ? callback(err) : callback(null, true)
+Paribasan.prototype.indexing = () => {
+  let paribasan_jawa_array = Paribasan._toArray(paribasan_jawa)
+  return new Promise((resolve, reject) => {
+    si.add(paribasan_jawa_array, function (err) {
+      err ? reject(err) : resolve(true)
+    })
   })
 }
 
-Paribasan.prototype.search = function (q, callback) {
-  var kata = {}
+Paribasan.prototype.search = (q) => {
+  let kata = {}
   kata.query = {'*': [q]}
-  si.search(kata, function (err, searchResult) {
-    if (!err) {
-      callback(null, searchResult)
-    } else {
-      callback(err)
-    }
+  return new Promise((resolve, reject) => {
+    si.search(kata, (err, searchResult) => {
+      err ? reject(err) : resolve(searchResult)
+    })
   })
 }
 
 Paribasan.prototype.get = function () {
-  var paribasan_array = Paribasan._toArray(paribasan_jawa)
-  var moratmarit = paribasan_jawa[Math.floor(Math.random() * paribasan_array.length)]
-  return '"' + moratmarit.paribasa.trim() + '" - ' + moratmarit.bahasa
+  let paribasan_array = Paribasan._toArray(paribasan_jawa)
+  let moratmarit = paribasan_jawa[Math.floor(Math.random() * paribasan_array.length)]
+  return `"${moratmarit.paribasa.trim()}" - ${moratmarit.bahasa}`
 }
 
-Paribasan._toArray = function (paribasan_object) {
-  return Object.keys(paribasan_object).map(function (k) {
+Paribasan._toArray = (paribasan_object) => {
+  return Object.keys(paribasan_object).map((k) => {
     return paribasan_object[k]
   })
 }
