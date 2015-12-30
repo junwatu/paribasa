2cc87d1077fa46e275c595fc25d1a491bb659556
Equan Pr <equan.p@gmail.com>
2015-12-30 14:05:36 +0700

    Add auto initialize indexing

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
 const os = require('os')
 const path = require('path')
 const tmp = os.tmpdir()
-const si = require('search-index')({indexPath: path.join(tmp, 'paribasa_index')})
+const indexTmp = path.join(tmp, 'paribasa_index')
+const si = require('search-index')({indexPath: indexTmp})
 
 let paribasan_jawa = require('./paribasa.json')
 
 function Paribasan () {
-  // lapo iki?
 }
 
-Paribasan.prototype.indexing = () => {
+Paribasan.init = () => {
   let paribasan_jawa_array = Paribasan._toArray(paribasan_jawa)
   return new Promise((resolve, reject) => {
     si.add(paribasan_jawa_array, (err) => {
@@ -20,22 +20,31 @@ Paribasan.prototype.indexing = () => {
   })
 }
 
+Paribasan.prototype.indexing = () => {
+  return new Promise((resolve, reject) => {
+    Paribasan.init().then(status => resolve(status), err => reject(err))
+  })
+}
+
 Paribasan.prototype.search = (q) => {
   let kata = {}
   kata.query = {'*': [q]}
+
   return new Promise((resolve, reject) => {
-    si.search(kata, (err, searchResult) => {
-      let hits = searchResult.hits
-      let search_array = []
-      hits.map((val) => {
-        search_array.push(`${val.document.paribasa} - ${val.document.bahasa}`)
+    Paribasan._checkIndexDir().then(() => {
+      si.search(kata, (err, searchResult) => {
+        let hits = searchResult.hits
+        let search_array = []
+        hits.map((val) => {
+          search_array.push(`${val.document.paribasa} - ${val.document.bahasa}`)
+        })
+        err ? reject(err) : resolve(search_array)
       })
-      err ? reject(err) : resolve(search_array)
     })
   })
 }
 
-Paribasan.prototype.get = function () {
+Paribasan.prototype.get = () => {
   let paribasan_array = Paribasan._toArray(paribasan_jawa)
   let moratmarit = paribasan_jawa[Math.floor(Math.random() * paribasan_array.length)]
   return `"${moratmarit.paribasa.trim()}" - ${moratmarit.bahasa}`
@@ -44,6 +53,19 @@ Paribasan.prototype.get = function () {
 Paribasan._toArray = (paribasan_object) => {
   return Object.keys(paribasan_object).map((k) => {
     return paribasan_object[k]
+  })
+}
+
+Paribasan._checkIndexDir = () => {
+  return new Promise((resolve, reject) => {
+    si.tellMeAboutMySearchIndex((info) => {
+      if (info.totalDocs === 0) {
+        console.log('Initialize indexing...')
+        Paribasan.init().then(() => resolve(true))
+      } else {
+        resolve(true)
+      }
+    })
   })
 }
 
