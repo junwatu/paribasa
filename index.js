000814ad40c0cab66a2f9801678acba1ca93ec2e
Equan Pr <equan.p@gmail.com>
2019-06-29 20:25:13 +0700

    Boring license

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 /**
- * Paribasa - Javanese Wisdom and Quote
- * (c) 2015-2019
  * 
- * MIT LICENSE
- */
+Paribasa - Javanese Wisdom and Quote
+ 
+MIT License
+
+Copyright 2015-2019 @junwatu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+software and associated documentation files (the "Software"), to deal in the Software 
+without restriction, including without limitation the rights to use, copy, modify, 
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies 
+or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE.
+
+
+#######################################################################################
+Intie sakarepmu tapi elokno lisensi ndek nduwur iku.
+#######################################################################################
+
+*/
 
 'use strict'
 const info = require('./package.json')
@@ -41,7 +65,6 @@ Paribasan.prototype.search = (q, cf) => {
             } else {
                 console.log(`${idx + 1}. ${gradient.summer(val.paribasa)} - ${gradient.vice(val.jawa)}`)
             }
-            
         })
     })
 }
